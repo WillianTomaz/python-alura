883dbfc4f3356fbdf0f3852cf61871e483b2fb2f
Willian Tomaz <willianlima28@hotmail.com>
2022-07-09 17:16:59 -0300

    Adicionando testes/exercicios do curso 'Python: Começando com a Linguagem'

---
+++ jogos/forca.py
@@ -0,0 +1,14 @@
+# forca.py
+
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+    
+
+# Essa variável '__name__', e ela é preenchida com o valor '__main__' caso o arquivo seja executado diretamente. 
+if (__name__ == "__main__"):
+    jogar()
