b9d57851808c15221a3523e96733177ebde4506a
Willian Tomaz <willianlima28@hotmail.com>
2022-07-13 14:22:02 -0300

    Pequenos ajustes

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,14 +1,34 @@
 # forca.py
 
 def jogar():
-
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
     
 
 # Essa variável '__name__', e ela é preenchida com o valor '__main__' caso o arquivo seja executado diretamente. 
-if (__name__ == "__main__"):
+if(__name__ == "__main__"):
     jogar()
