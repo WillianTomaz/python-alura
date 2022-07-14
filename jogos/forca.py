8e7b039c82b638e5b6d1cc89ffbd6ea7546c1ecb
Willian Tomaz <willianlima28@hotmail.com>
2022-07-14 15:25:40 -0300

    feat: Ajustando jogo para pegar palavra secreta dinamica

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,8 +5,8 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
@@ -26,19 +26,22 @@ def jogar():
                     letras_acertadas[index] = letra
                 index += 1
         else:
-                erros += 1
-
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
 
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-    
+
+
     if(acertou):
-        print("Você ganhou!")
+        print("Você ganhou!!")
     else:
-        print("Você Perdeu!")
+        print("Você perdeu!!")
 
     print("Fim do jogo")
+
+
 # Essa variável '__name__', e ela é preenchida com o valor '__main__' caso o arquivo seja executado diretamente. 
 if(__name__ == "__main__"):
     jogar()
