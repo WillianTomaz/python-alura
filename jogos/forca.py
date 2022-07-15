537afc32e673ce2dbfeffff4f2065645c0df887a
Willian Tomaz <willianlima28@hotmail.com>
2022-07-14 21:05:18 -0300

    feat: Melhorando a legibilidade e organização do código

--- jogos/forca.py
+++ jogos/forca.py
@@ -2,22 +2,10 @@
 import random
 
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -51,6 +39,32 @@ def jogar():
         print("Você perdeu!!")
 
     print("Fim do jogo")
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
+
 
 
 # Essa variável '__name__', e ela é preenchida com o valor '__main__' caso o arquivo seja executado diretamente. 
