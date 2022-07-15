67941fb1f67435372cf9814da196cbb5cb722104
Willian Tomaz <willianlima28@hotmail.com>
2022-07-15 15:45:00 -0300

    feat: Adicionando exemplos de código com PEP-8 e Type hints

---
+++ boas-praticas-pep-8/pep-8-typing/main.py
@@ -0,0 +1,22 @@
+from fila_normal import filanormal
+from fila_prioritaria import FilaPrioritaria
+
+
+# Exemplos de código padrões
+# fila_teste = filanormal()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# print(fila_teste.chamacliente(5))
+# print(fila_teste.chamacliente(10))
+
+
+
+# Exemplos de código com PEP-8 e Type hints
+fila_teste_2 = FilaPrioritaria()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+print(fila_teste_2.chama_cliente(5))
+print(fila_teste_2.chama_cliente(1))
+print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
