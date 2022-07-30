9ed55a7b9ca1e9b5c54e608eeffc072404b81743
Willian Tomaz <willianlima28@hotmail.com>
2022-07-30 11:25:53 -0300

    feat: Trabalhando com Herança (Reutilização de Código)

--- orientacao-a-objetos/orientacao-a-objetos-2/modelo.py
+++ orientacao-a-objetos/orientacao-a-objetos-2/modelo.py
@@ -1,22 +1,33 @@
-class Filme:
+class Programa:
+  def __init__(self, nome, ano):
+    self._nome = nome.title()
+    self.ano = ano
+    self._likes = 0
+
+  @property
+  def nome(self):
+    return self._nome
+  
+  @nome.setter
+  def nome(self, novo_nome):
+    self._nome = novo_nome.title()
+  
+  @property
+  def likes(self):
+    self._likes += 1
+
+  def dar_likes(self):
+    self._likes += 1
+
+class Filme (Programa):
   def __init__(self, nome, ano, duracao):
-    self.nome = nome.title()
-    self.ano = ano
+    super().__init__(nome, ano)
     self.duracao = duracao
-    self.likes = 0
 
-  def dar_likes(self):
-    self.likes += 1
-
-class Serie:
+class Serie (Programa):
   def __init__(self, nome, ano, temporadas):
-    self.nome = nome.title()
-    self.ano = ano
+    super().__init__(nome, ano)
     self.temporadas = temporadas
-    self.likes = 0
-
-  def dar_likes(self):
-    self.likes += 1
 
 
 vingadores = Filme('vingadores - gerra infinita', 2018, 160)
