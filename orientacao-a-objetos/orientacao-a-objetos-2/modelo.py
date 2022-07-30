c6f1a33a9962142a7ea1333489ec621b570f9a9b
Willian Tomaz <willianlima28@hotmail.com>
2022-07-30 19:41:35 -0300

    feat: Removendo Complexidade de código e Trabalhando com Polimorfismo

--- orientacao-a-objetos/orientacao-a-objetos-2/modelo.py
+++ orientacao-a-objetos/orientacao-a-objetos-2/modelo.py
@@ -19,34 +19,37 @@ class Programa:
   def dar_likes(self):
     self._likes += 1
 
+  def imprime(self):
+    print(f'{self._nome} - {self.ano} - {self._likes} Likes')
+
 class Filme (Programa):
   def __init__(self, nome, ano, duracao):
     super().__init__(nome, ano)
     self.duracao = duracao
+
+  def imprime(self):
+    print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
 
 class Serie (Programa):
   def __init__(self, nome, ano, temporadas):
     super().__init__(nome, ano)
     self.temporadas = temporadas
 
-# ----------------------------------------------
-print("-----------------")
-vingadores = Filme('vingadores - gerra infinita', 2018, 160)
-vingadores.dar_likes()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duração: {vingadores.duracao} - Temporada: {vingadores.likes}')
+  def imprime(self):
+    print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes')
+
 
 # ----------------------------------------------
-print("-----------------")
+vingadores = Filme('vingadores - gerra infinita', 2018, 160)
+vingadores.dar_likes()
+
+# ----------------------------------------------
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporada: {atlanta.temporadas} - Temporada: {atlanta.likes}')
 
 # ----------------------------------------------
-print("-----------------")
 filmes_e_series = [vingadores, atlanta]
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
+  print("-----------------")
+  programa.imprime()
