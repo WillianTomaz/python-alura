2bf930b338615417c0d6f3f7b1754301af7e2cca
Willian Tomaz <willianlima28@hotmail.com>
2022-07-29 23:18:05 -0300

    feat: Adicionando metodo dar_likes()

--- orientacao-a-objetos/orientacao-a-objetos-2/modelo.py
+++ orientacao-a-objetos/orientacao-a-objetos-2/modelo.py
@@ -1,22 +1,32 @@
 class Filme:
   def __init__(self, nome, ano, duracao):
-    self.nome = nome
+    self.nome = nome.title()
     self.ano = ano
     self.duracao = duracao
+    self.likes = 0
 
+  def dar_likes(self):
+    self.likes += 1
 
 class Serie:
   def __init__(self, nome, ano, temporadas):
-    self.nome = nome
+    self.nome = nome.title()
     self.ano = ano
     self.temporadas = temporadas
+    self.likes = 0
+
+  def dar_likes(self):
+    self.likes += 1
 
 
-vingadores = Filme('vingadores', 2018, 160)
+vingadores = Filme('vingadores - gerra infinita', 2018, 160)
+vingadores.dar_likes()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duração: {vingadores.duracao}')
+      f'- Duração: {vingadores.duracao} - Temporada: {vingadores.likes}')
 
 
 atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_likes()
+atlanta.dar_likes()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Duração: {atlanta.temporadas}')
+      f'- Temporada: {atlanta.temporadas} - Temporada: {atlanta.likes}')
