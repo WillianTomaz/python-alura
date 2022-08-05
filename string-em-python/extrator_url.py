67a22fea9512766cfd6657d2729af198aa4c1f1a
Willian Tomaz <willianlima28@hotmail.com>
2022-08-05 09:07:47 -0300

    feat: Fazendo validação da URL com RegEx e adicionando outros exemplos de código

--- string-em-python/extrator_url.py
+++ string-em-python/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
   def __init__(self, url):
     self.url = self.sanitiza_url(url)
@@ -10,8 +12,12 @@ class ExtratorURL:
       return ""
 
   def valida_url(self):
+    padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+    match = padrao_url.match(self.url)
     if not self.url:
       raise ValueError("A URL está vazia")
+    if not match:
+        raise ValueError("A URL não é válida.")
 
   def get_url_base(self):
     indice_interrogacao = self.url.find('?')
@@ -32,9 +38,3 @@ class ExtratorURL:
     else:
         valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
     return valor
-
-
-# extrator_url = ExtratorURL("  ")
-extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
