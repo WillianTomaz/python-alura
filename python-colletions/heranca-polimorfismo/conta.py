c41a57ee39ca64e5c8ee974707585fad1c0276aa
Willian Tomaz <willianlima28@hotmail.com>
2022-08-08 19:37:47 -0300

    feat: Adicionando exemplo de abstractmethod

--- python-colletions/heranca-polimorfismo/conta.py
+++ python-colletions/heranca-polimorfismo/conta.py
@@ -1,4 +1,6 @@
-class Conta:
+from abc import ABCMeta, abstractmethod
+
+class Conta(metaclass=ABCMeta):
 
   def __init__(self, codigo):
     self._codigo = codigo
@@ -6,6 +8,13 @@ class Conta:
 
   def deposita(self, valor):
     self._saldo += valor
+
+  # Forçando a implementação 
+  # se não fazer implementação do método, 
+  # na hora de instaciar vai dar erro
+  @abstractmethod
+  def passa_o_mes(self):
+    pass
 
   def __str__(self):
     return f"Codigo: {self._codigo} | Saldo: {self._saldo}"
